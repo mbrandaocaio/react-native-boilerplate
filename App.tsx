cb19eff251c9c1abc4028f007691a25845479e62
mbrandaocaio <mbrandao.caio@gmail.com>
2023-02-21 16:36:36 -0300

    removing one signal

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import OneSignal from 'react-native-onesignal';
 import {Routes} from './src/routes';
-
-OneSignal.setAppId('id do one signal');
 
 const App = () => {
   return <Routes />;
